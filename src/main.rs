4ba386acf5359c8db4e1e6f697f21707d2263695
Sedat Kapanoglu <sedat@kapanoglu.com>
2025-01-30 13:11:47 -0800

    upgrade to clap

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,59 @@
 use std::{fs::File, process::ExitCode};
 
 use act::ReadError;
-use gumdrop::Options;
+use clap::Parser;
 
 mod act;
 mod txt;
 
-#[derive(Options, Debug)]
+#[derive(Parser, Debug)]
 struct Params {
-    #[options(help = "Print this help message")]
-    help: bool,
-
-    #[options(short = "f", no_long, help = "Overwrite <output> if it exists")]
+    #[arg(short = 'f', long = "force", help = "Overwrite <output> if it exists")]
     overwrite: bool,
 
-    #[options(no_short, long = "all", help = "Force convert all colors (even if extra data indicates otherwise)")]
+    #[arg(long = "all", help = "Force convert all colors (even if extra data indicates otherwise)")]
     all: bool,
 
-    #[options(free, required, help = "The input file to use (ACT format)")]
+    #[arg(help = "The input file to use (ACT format)")]
     input: String,
 
-    #[options(free, required, help = "The output file to write (TXT format)")]
+    #[arg(help = "The output file to write (Paint.NET TXT format)")]
     output: String,
 }
 
-fn err(msg: String) -> ExitCode {
-    println!("{}", msg);
-    ExitCode::FAILURE
-}
-
-fn usage() -> ExitCode {
-    err(Params::usage().to_string())
+macro_rules! failure {
+    ($arg:expr) => {{
+        eprintln!("{}", $arg);
+        ExitCode::FAILURE
+    }};
+    ($($arg:tt)+) => {{
+        eprintln!($($arg)+);
+        ExitCode::FAILURE
+    }};
 }
 
 fn main() -> ExitCode {
-    let cmd_args = std::env::args().skip(1).collect::<Vec<String>>();
-    let Ok(args) = Params::parse_args_default(&cmd_args) else {
-        return usage();
-    };
-
-    if args.help {
-        return usage();
-    }
+    let args = Params::parse();
 
     let Ok(mut in_file) = File::open(&args.input) else {
-        return err(format!("Could not open input file: {}", args.input));
+        return failure!("Could not open input file: {}", args.input);
     };
 
     let palette = match act::Palette::read(&mut in_file, args.all) {
         Ok(p) => p,
         Err(e) => return match e {
-            ReadError::InvalidFileLength => err(format!("Invalid input file length: {}", args.input)),
-            ReadError::IoError => err(format!("Could not read input file: {}", args.input)),
+            ReadError::InvalidFileLength => failure!("Invalid input file length: {}", args.input),
+            ReadError::IoError => failure!("Could not read input file: {}", args.input),
         }    
     };
 
     let result = if args.overwrite { File::create(&args.output) } else { File::create_new(&args.output) };    
     let Ok(mut out_file) = result else {
-        return err(format!("Could not create file: {}", args.output));
+        return failure!("Could not create file: {}", args.output);
     };
 
     if palette.write_pdn_txt(&mut out_file).is_err() {
-        return err(format!("Error: Could not write palette to output file {}", args.output));
+        return failure!("Could not write palette to output file {}", args.output);
     }
 
     ExitCode::SUCCESS
